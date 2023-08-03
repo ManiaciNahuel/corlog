bf384bbd5558fea92fc922658754f1c5f651e741
Nahuel <nahuelmaniaci@gmail.com>
2023-08-03 10:51:29 -0300

    Hamburguer menu

--- app.js
+++ app.js
@@ -2,6 +2,19 @@ const root = document.documentElement;
 const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue("--marquee-elements-displayed");
 const marqueeContent = document.querySelector("ul.marquee-content");
 
+function openNav() {
+    document.getElementById("mySidenav").style.visibility = "visible";
+    document.getElementById("mySidenav").style.opacity = "1";
+    document.getElementById("header_1").style.opacity = "0.7";
+    document.getElementById("main").style.opacity = "0.7";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.visibility = "hidden";
+    document.getElementById("header_1").style.opacity = "1";
+    document.getElementById("main").style.opacity = "1";
+}
+
 root.style.setProperty("--marquee-elements", marqueeContent.children.length);
 
 for (let i = 0; i < marqueeElementsDisplayed; i++) {
@@ -9,10 +22,10 @@ for (let i = 0; i < marqueeElementsDisplayed; i++) {
 }
 
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip "]'))
-var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
     return new bootstrap.Tooltip(tooltipTriggerEl)
 })
 var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover "]'))
-var popoverList = popoverTriggerList.map(function(popoverTriggerEl) {
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
     return new bootstrap.Popover(popoverTriggerEl)
 })
